503dd99c8cd5c23c3b1a3dc4b1e4bea47b600f06
Joseph <jmello@ucsc.edu>
2022-12-06 11:00:40 -0800

    renamed function file, removed copied class, and added comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,23 @@
-//import logo from './logo.svg';
 import React from 'react';
-//import "./button.css";
 import './App.css';
-import Button from './button';
+import FloorPrice from './FloorPrice';
 
 const App = () => {
-  var dao = Button('linksdao');
-  var champ = Button('linksdao-champions');
+  //initialize the floor price of linksdao and links champions
+  //to follow hook call syntax (Hooks can only be called in the beginning of a function/hook)
+  var dao = FloorPrice('linksdao');
+  var champ = FloorPrice('linksdao-champions');
+  //empty handle event for when button1 is clicked
   const handleMenuOne = () => {
     console.log('clicked one');
   };
-
+  //empty handle event for when button2 is clicked
   const handleMenuTwo = () => {
-    
+    console.log('clicked two');
   };
 
   return (
+    //return JSX with a trigger for Floor Price
     <Dropdown
       trigger={<button>Floor Price</button>}
       menu={[
@@ -25,14 +27,19 @@ const App = () => {
     />
   );
 };
+
+//create a function for a dropdown menu
 const Dropdown = ({ trigger, menu }) => {
+  //use hook to set the open state to closed
   const [open, setOpen] = React.useState(false);
 
+  //to open and close dropdown menu when clicked
   const handleOpen = () => {
     setOpen(!open);
   };
 
   return(
+    //JSX for dropdown menu
     <div className="dropdown">
       {React.cloneElement(trigger, {
         onClick: handleOpen,
